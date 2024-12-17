852b9c5a8d9850432c360b0cbbe0f84d216f63cd
tianlin0 <tianlin0@qq.com>
2024-12-17 14:23:45 +0800

    add context.go

--- goroutines/context.go
+++ goroutines/context.go
@@ -3,27 +3,52 @@ package goroutines
 import (
 	"context"
 	"github.com/go-eden/routine"
-	"sync"
+	gocache "github.com/patrickmn/go-cache"
+	"strconv"
+	"time"
 )
 
-var ctxMap sync.Map
+var (
+	expiration      = 20 * time.Minute
+	cleanupInterval = 30 * time.Minute
+	baseInt         = 10
+)
+
+var ctxCache *gocache.Cache
+
+func getCache() *gocache.Cache {
+	if ctxCache == nil {
+		ctxCache = gocache.New(expiration, cleanupInterval)
+	}
+	return ctxCache
+}
+func getGoId() string {
+	return strconv.FormatInt(routine.Goid(), baseInt)
+}
 
 // SetContext 设置上下文
 func SetContext(ctx *context.Context) {
-	ctxMap.Store(routine.Goid(), ctx)
+	ctxFactory := getCache()
+	ctxFactory.Set(getGoId(), ctx, gocache.DefaultExpiration)
 }
 
 // GetContext 获取上下文
 func GetContext() (ctx *context.Context) {
-	val, ok := ctxMap.Load(routine.Goid())
+	ctxFactory := getCache()
+
+	val, ok := ctxFactory.Get(getGoId())
 	if !ok {
 		return nil
 	}
-	ctx, _ = val.(*context.Context)
-	return
+	ctx, ok = val.(*context.Context)
+	if ok {
+		return ctx
+	}
+	return nil
 }
 
 // DelContext 删除上下文
 func DelContext() {
-	ctxMap.Delete(routine.Goid())
+	ctxFactory := getCache()
+	ctxFactory.Delete(getGoId())
 }
